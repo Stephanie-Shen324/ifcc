0516ee1050dda6e71e229c463519ad8aaf8f16e4
juneyaooo <juneyaooo@gmail.com>
2021-05-25 11:32:52 +0800

    adjust seed

--- train.py
+++ train.py
@@ -36,6 +36,9 @@ def main(args):
     random.seed(args.seed)
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
     # Embeddings
     embeddings, word_idxs = PretrainedEmbeddings.load_embeddings(args.embeddings)
     # Data
